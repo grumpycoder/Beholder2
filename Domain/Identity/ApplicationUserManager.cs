ce618786a1a1bfe694214b369ec478fb3928c92e
Mark Lawrence <grumpycoder@outlook.com>
2015-10-08 09:03:28 -0500

    added applicationusermanager to domain with uservalidator and passwordvalidator

---
+++ Domain/Identity/ApplicationUserManager.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Models;
+using Microsoft.AspNet.Identity;
+
+namespace Domain.Identity
+{
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+
+        public ApplicationUserManager(IUserStore<ApplicationUser> store): base(store)
+        {
+            UserValidator = new UserValidator<ApplicationUser>(this)
+            {
+                AllowOnlyAlphanumericUserNames = false, 
+                RequireUniqueEmail = true,
+            };
+            PasswordValidator = new PasswordValidator()
+            {
+                RequireDigit = false,
+                RequireNonLetterOrDigit = false,
+                RequireUppercase = false, 
+                RequiredLength = 3
+            };
+        }
+
+    }
+}
